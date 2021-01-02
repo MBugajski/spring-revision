15f337b743f4a3191f94ce8a5b8eed0360423f22
MBugajski <michalbugajski@outlook.com>
2021-01-02 23:37:19 +0100

    Stream with collections

--- spring-revision-stream/src/main/java/com/MBugajski/stream/StreamMain.java
+++ spring-revision-stream/src/main/java/com/MBugajski/stream/StreamMain.java
@@ -1,8 +1,15 @@
 package com.MBugajski.stream;
 
 import com.MBugajski.stream.beautifier.PoemBeautifier;
+import com.MBugajski.stream.book.Book;
+import com.MBugajski.stream.book.BookDirectory;
 import com.MBugajski.stream.lambda.ExpressionExecutor;
+import com.MBugajski.stream.person.People;
 import com.MBugajski.stream.reference.FunctionalCalculator;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -20,12 +27,44 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
-        PoemBeautifier poemBeautifier= new PoemBeautifier();
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
         System.out.println("Beautifying sentences");
         poemBeautifier.beautify("Roses are red.", a -> a.toUpperCase());
         poemBeautifier.beautify("Roses are red.", a -> a.toLowerCase());
-        poemBeautifier.beautify("Roses are red.", a -> a+a);
+        poemBeautifier.beautify("Roses are red.", a -> a + a);
         poemBeautifier.beautify("Roses are red.", a -> a + " Violets are blue.");
 
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
+                .forEach(System.out::println);
+
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n", "<<", ">>"));
+
+        System.out.println(theResultStringOfBooks);
     }
 }
+
